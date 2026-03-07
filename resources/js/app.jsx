c5f185b66d42c5ba025d4829bc29978dd5f13add
TyG Test <sulkerkiwe@gmail.com>
2026-03-08 03:34:09 +0700

    First commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+
+createInertiaApp({
+  resolve: (name) => {
+    const pages = import.meta.glob("./pages/**/*.jsx", { eager: true });
+    return pages[`./pages/${name}.jsx`];
+  },
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />);
+  },
+});
---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Cashier</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    @inertiaHead
+</head>
+<body>
+    @inertia
+</body>
+</html>
